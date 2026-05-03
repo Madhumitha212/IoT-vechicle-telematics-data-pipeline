6c7ad8ba9e62c85d4389dfda5798c3a670744522
Madhumitha212 <madhuravi324@gmail.com>
2026-05-03 09:04:27 +0530

    add: [Madhu]-added redshift client setup with environment variables

---
+++ config/get_client.py
@@ -0,0 +1,24 @@
+import boto3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+REDSHIFT_DATABASE = os.getenv("REDSHIFT_DATABASE")
+REDSHIFT_DB_USER = os.getenv("REDSHIFT_DB_USER")
+REDSHIFT_WORKGROUP = os.getenv("REDSHIFT_WORKGROUP")
+REDSHIFT_IAM_ROLE_ARN = os.getenv("REDSHIFT_IAM_ROLE_ARN")
+
+AWS_REGION = os.getenv("AWS_REGION")
+BASE_S3 = os.getenv("BASE_S3")
+
+_redshift_client = None
+
+def get_redshift_client():
+    global _redshift_client
+    if _redshift_client is None:
+        _redshift_client = boto3.client(
+            "redshift-data",
+            region_name=AWS_REGION
+        )
+    return _redshift_client
